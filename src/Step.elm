33ce2cf3567f6607d7bf524c3f1c2be4ea0fbf4c
Austin Erlandson <austin@erlandson.com>
2019-04-20 22:33:06 -0500

    Update Step.elm

--- src/Step.elm
+++ src/Step.elm
@@ -1,100 +1,111 @@
-module Step exposing
-    ( Step, to, stay, exit, fromUpdate, fromMaybe
-    , withCmd, withAttempt, command, attempt
-    , map, mapMsg, within, mapExit, orElse, onExit
-    , run, asUpdateFunction
-    , foldSteps
+module Pin exposing
+    ( Note
+    , write, g, pong, wait
+    , edit, gWith, pongWith, onPong
+    , wrap
+    , run
+    , tack
+    , gAttempt
+    , foldNotes
     )
 
-{-|
+{-| import Pin exposing
+( Note
+, pong
+, pongWith
+, onPong
+)
 
 
-# Steps and how to make them
+# Notes and how to make them
 
-@docs Step, to, stay, exit, fromUpdate, fromMaybe
-
-
-# Issuing commands
-
-@docs withCmd, withAttempt, command, attempt
+@docs Note
+@docs write, g, pong, wait
 
 
-# Transforming and Composing Steps
+# Transforming and Composing Notes
 
 All of these functions help you build functions that return steps out of other functions that return steps
 
-@docs map, mapMsg, within, mapExit, orElse, onExit
+@docs edit, gWith, pongWith, onPong
+@docs wrap, wrapWith
 
 
 # Getting back to TEA land
 
-@docs run, asUpdateFunction
+@docs run
+@docs tack
+
+
+# In case of you're using a Task???
+
+@docs gAttempt
 
 
 # Testing update functions
 
-@docs foldSteps
+@docs foldNotes
 
 -}
 
-import Task
+import Task exposing (Task)
 
 
-{-| A `Step model msg a` describes one state transition of an application, and is intended to be what gets returned from an update function.
+{-| A `Note from to content` describes one state transition of an application, and is intended to be what gets returned from an update function.
 
-It's helpful to look at how a `Step` is (roughly) represented under the hood
+It's helpful to look at how a `Note` is (roughly) represented under the hood
 
-    type Step model msg a
-        = To model (Cmd msg)
-        | Exit a
-        | Stay
+    type Note from to content
+        = Write (List (Cmd to)) content
+        | Recieve from
+        | Wait
 
-We provide a smart constructor for each of these variants, but we hide the internal representation to make sure you're not pattern matching on `Step`s willy-nilly.
+We provide a smart constructor for each of these variants, but we hide the internal representation to make sure you're not pattern matching on `Note`s willy-nilly.
 
 That being said, if you find something that makes you want the data structure fully exposed, please make an issue on GitHub!
 
 -}
-type Step model msg a
-    = To model (List (Cmd msg))
-    | Exit a
-    | Stay
+type Note from to content
+    = Write (List (Cmd to)) content
+    | Recieve from
+    | Wait
 
 
 {-| Transition to a new state, without executing any commands
 -}
-to : model -> Step model msg a
-to state =
-    To state []
+write : content -> Note from to content
+write =
+    Write []
+
+
+{-| Stop the interaction by returning a value of type `a`
+-}
+pong : from -> Note from to content
+pong =
+    Recieve
 
 
 {-| Keep the interaction in the state it was in.
 
 **Note**: This will prevent any commands from being returned
 
-    Step.stay == (Step.stay |> Step.withCmd myHttpCall)
+    Pin.wait == (Pin.wait |> Pin.gWith myHttpCall)
 
-If you want to stay in the same state, but run some commands, use `Step.to` explicitly
+If you want to empty in the same state, but run some commands, use `Pin.to` explicitly
 
-    Step.to MySameState |> Step.withCmd myHttpCall
+    Pin.to MySameState |> Note.gWith myHttpCall
 
 -}
-stay : Step model msg a
-stay =
-    Stay
-
-
-{-| End the interaction by returning a value of type `a`
--}
-exit : a -> Step model msg a
-exit =
-    Exit
+wait : Note from to content
+wait =
+    Wait
 
 
 {-| If we're stepping `to` a new state, add an cmd to fire off
 
-This can be called on a `Step` multiple times, and all the commands will fire.
+This can be called on a `Note` multiple times, and all the commands will fire.
 
-No commands are fired if the Step turns out to be a `stay` or an `exit`
+No commands are fired if the Note turns out to be a `empty` or an `from`
 
 Alternate name ideas:
 
@@ -105,330 +116,174 @@ Alternate name ideas:
   - with
 
 -}
-withCmd : Cmd msg -> Step model msg a -> Step model msg a
-withCmd cmd step =
+g : Cmd to -> Note from to content -> Note from to content
+g cmd step =
     case step of
-        To state commands ->
-            To state (cmd :: commands)
+        Write commands state ->
+            Write (cmd :: commands) state
 
-        Exit o ->
-            Exit o
+        Recieve from ->
+            Recieve from
 
-        Stay ->
-            Stay
-
-
-{-| Experimental alias for withCmd
--}
-command : Cmd msg -> Step model msg a -> Step model msg a
-command cmd step =
-    case step of
-        To state commands ->
-            To state (cmd :: commands)
-
-        Exit o ->
-            Exit o
-
-        Stay ->
-            Stay
+        Wait ->
+            Wait
 
 
-{-| Apply a function to the state inside a `Step`, if it's there
+{-| Apply a function to the state inside a `Note`, if it's there
 
-Most useful for building a `Step` out of another `Step` returned from some other update function you're calling
+Most useful for building a `Note` out of another `Note` returned from some other update function you're calling
 
 -}
-map : (model1 -> model2) -> Step model1 msg a -> Step model2 msg a
-map f step =
+edit : (content1 -> content2) -> Note from to content1 -> Note from to content2
+edit f step =
     case step of
-        To state cmd ->
-            To (f state) cmd
+        Write cmd state ->
+            Write cmd (f state)
 
-        Exit o ->
-            Exit o
+        Recieve from ->
+            Recieve from
 
-        Stay ->
-            Stay
+        Wait ->
+            Wait
 
 
-{-| Apply a function to any `msg`s in the `Step`'s commands
+{-| Apply a function to any `msg`s in the `Note`'s commands
 
-Often used alongside `map` for building "bigger" `Step`s out of "smaller" ones
+Often used alongside `map` for building "bigger" `Note`s out of "smaller" ones
 
 -}
-mapMsg : (msg1 -> msg2) -> Step model msg1 a -> Step model msg2 a
-mapMsg f step =
+gWith : (to1 -> to2) -> Note from to1 content -> Note from to2 content
+gWith f step =
     case step of
-        To state msgCmd ->
-            To state (List.map (Cmd.map f) msgCmd)
+        Write msgCmd state ->
+            Write (List.map (Cmd.map f) msgCmd) state
 
-        Exit o ->
-            Exit o
+        Recieve from ->
+            Recieve from
 
-        Stay ->
-            Stay
-
-
-{-| Run the first suceeding `Step`, with priority given to the second argument
-
-Intended to be used pipeline style
-
-    Step.to { loading = True } |> Step.orElse (Step.to { loading = False }) == Step.to { loading = True }
-
-    Step.noop |> Step.orElse Step.to { loading = True } == Step.to { loading = True }
-
--}
-orElse : Step model msg a -> Step model msg a -> Step model msg a
-orElse stepA stepB =
-    case ( stepA, stepB ) of
-        ( To _ _, To state commands ) ->
-            To state commands
-
-        ( To state commands, Stay ) ->
-            To state commands
-
-        ( To state commands, Exit o ) ->
-            Exit o
-
-        ( Stay, To state commands ) ->
-            To state commands
-
-        ( Stay, Stay ) ->
-            Stay
-
-        ( Stay, Exit o ) ->
-            Exit o
-
-        ( Exit o, To state commands ) ->
-            Exit o
-
-        ( Exit o, Stay ) ->
-            Exit o
-
-        ( Exit _, Exit o ) ->
-            Exit o
+        Wait ->
+            Wait
 
 
 {-| Map over the output of an interaction, if we've reached the end
 -}
-mapExit : (a -> b) -> Step model msg a -> Step model msg b
-mapExit f step =
+pongWith : (fromA -> fromB) -> Note fromA to content -> Note fromB to content
+pongWith fn step =
     case step of
-        To state commands ->
-            To state commands
+        Write to content ->
+            Write to content
 
-        Exit o ->
-            Exit (f o)
+        Recieve fromA ->
+            Recieve (fn fromA)
 
-        Stay ->
-            Stay
+        Wait ->
+            Wait
 
 
-{-| Choose a `Step` based on the result of another interaction
+{-| Choose a `Note` based on the result of another interaction
 
 You can use this in combination with `map` and `mapMsg` to glue the end of one interaction to the beginning of another.
 
 Notice that it looks a lot like `Maybe.andThen` and `Result.andThen`, but operating on the last type variable.
 
 -}
-onExit : (a -> Step model msg b) -> Step model msg a -> Step model msg b
-onExit f step =
+onPong : (fromA -> Note fromB to content) -> Note fromA to content -> Note fromB to content
+onPong f step =
     case step of
-        To state commands ->
-            To state commands
+        Write to content ->
+            Write to content
 
-        Exit o ->
-            f o
+        Recieve fromA ->
+            f fromA
 
-        Stay ->
-            Stay
+        Wait ->
+            Wait
 
 
-{-| Turn a `Step` into the usual TEA update tuple
+{-| Turn a `Note` into the usual TEA update tuple
 
-It must be a `Step` that doesn't `exit`. We know it is if the type variable `a` is still lowercase, i.e. not a specifc type, and can thus be chosen to be `Never` when calling this function.
+It must be a `Note` that doesn't `from`. We know it is if the type variable `a` is still lowercase, i.e. not a specifc type, and can thus be chosen to be `Never` when calling this function.
 
 -}
-run : Step model msg Never -> Maybe ( model, Cmd msg )
+run : Note Never to content -> Maybe ( content, Cmd to )
 run s =
     case s of
-        To state commands ->
+        Write commands state ->
             Just ( state, Cmd.batch commands )
 
-        Exit n ->
+        Recieve n ->
             never n
 
-        Stay ->
+        Wait ->
             Nothing
 
 
-{-| Turn an update function that returns a `Step` to a normal Elm Architecture update function
+{-| Turn an update function that returns a `Note` to a normal Elm Architecture update function
 
-uses `run` internally to default with the provided model in case of a `stay`
+uses `run` internally to default with the provided content in case of a `empty`
 
 -}
-asUpdateFunction : (msg -> model -> Step model msg Never) -> msg -> model -> ( model, Cmd msg )
-asUpdateFunction update msg model =
-    update msg model
+tack : (to -> content -> Note Never to content) -> to -> content -> ( content, Cmd to )
+tack update msg content =
+    update msg content
         |> run
-        |> Maybe.withDefault ( model, Cmd.none )
+        |> Maybe.withDefault ( content, Cmd.none )
 
 
-filterMap : (x -> Maybe y) -> Step x msg a -> Step y msg a
-filterMap f step =
+{-| A helper for building `Note`s out of tasks
+-}
+gAttempt : (Result err val -> to) -> Task err val -> Note from to content -> Note from to content
+gAttempt handler task step =
     case step of
-        To state cmds ->
-            case f state of
-                Just newState ->
-                    To newState cmds
+        Write cmds state ->
+            Write (Task.attempt handler task :: cmds) state
 
-                Nothing ->
-                    Stay
+        Recieve from ->
+            Recieve from
 
-        Stay ->
-            Stay
-
-        Exit o ->
-            Exit o
-
-
-{-| Step to the state denoted by the `model` in the `Just` case, stay otherwise
--}
-fromMaybe : Maybe model -> Step model msg a
-fromMaybe x =
-    -- TODO rename
-    case x of
-        Just s ->
-            To s []
-
-        Nothing ->
-            Stay
-
-
-{-| Build a `Step` from a normal elm update tuple
--}
-fromUpdate : ( model, Cmd msg ) -> Step model msg o
-fromUpdate ( s, cmd ) =
-    To s [ cmd ]
-
-
-{-| A helper for building `Step`s out of tasks
--}
-withAttempt : (Result x a -> msg) -> Task.Task x a -> Step model msg b -> Step model msg b
-withAttempt handler task step =
-    case step of
-        To state cmds ->
-            To state (Task.attempt handler task :: cmds)
-
-        Stay ->
-            Stay
-
-        Exit o ->
-            Exit o
-
-
-{-| Experimental alias for `withAttempt`
--}
-attempt : (Result x a -> msg) -> Task.Task x a -> Step model msg b -> Step model msg b
-attempt handler task step =
-    case step of
-        To state cmds ->
-            To state (Task.attempt handler task :: cmds)
-
-        Stay ->
-            Stay
-
-        Exit o ->
-            Exit o
+        Wait ->
+            Wait
 
 
 {-| Starting from an initial state, fold an update function over a list of messages
 
-Only use this to test that the application of certain messages produces the result you expect. In application code, building up a bunch of `Msg`s just to feed them to an update function is ususally not worth the effort.
+Only use this to test that the application of certain messages produces the result you expect.
+In application code, building up a bunch of `Msg`s just to feed them to an update function is ususally not worth the effort.
 
 -}
-foldSteps :
-    (msg -> model -> Step model msg a)
-    -> ( model, Cmd msg )
+foldNotes :
+    (msg -> content -> Note from to content)
+    -> ( content, Cmd to )
     -> List msg
-    -> Step model msg a
-foldSteps update init msgs =
+    -> Note from to content
+foldNotes update init msgs =
+    let
+        andThen : (content1 -> Note from to content2) -> Note from to content1 -> Note from to content2
+        andThen f s =
+            case s of
+                Write commands state ->
+                    f state |> g (Cmd.batch commands)
+
+                Recieve from ->
+                    Recieve from
+
+                Wait ->
+                    Wait
+
+        -- Build a `Note` from a normal elm update tuple
+        fromUpdate : ( content, Cmd to ) -> Note from to content
+        fromUpdate ( content, to ) =
+            Write [ to ] content
+    in
     {- TODO rename -}
     List.foldl (andThen << update) (fromUpdate init) msgs
 
 
-andThen : (model1 -> Step model2 msg o) -> Step model1 msg o -> Step model2 msg o
-andThen f s =
-    case s of
-        To state commands ->
-            f state |> withCmd (Cmd.batch commands)
-
-        Stay ->
-            Stay
-
-        Exit o ->
-            Exit o
-
-
-oneOf : List (Step model msg a) -> Step model msg a
-oneOf steps =
-    case steps of
-        [] ->
-            Stay
-
-        head :: tail ->
-            List.foldl orElse head tail
-
-
 {-| Use a step "within" a larger interaction
 
-    Step.within f g = Step.map f >> Step.mapMsg g
+    Note.within f g = Note.map f >> Note.mapMsg g
 
 -}
-within : (model1 -> model2) -> (msg1 -> msg2) -> Step model1 msg1 a -> Step model2 msg2 a
-within f g =
-    map f >> mapMsg g
-
-
-withCmds : List (Cmd msg) -> Step model msg a -> Step model msg a
-withCmds a =
-    withCmd (Cmd.batch a)
-
-
-{-| Step to a new state and cmd the
-
-    Step.lead
-
-    Step.to Loading
-        |> Step.withCmd httpRequest
-        |> Step.cmd httpRequest
-
-    Step.to Loading
-        |> Step.cmd httpRequest
-
-    Step.to Loading
-        |> Step.cmd httpRequest
-
-    Step.withCmd httpRequest Loading
-
--}
-withCmd_ : Cmd msg -> model -> Step model msg a
-withCmd_ cmd model =
-    To model [ cmd ]
-
-
-when : (model -> Bool) -> Step model msg a -> Step model msg a
-when predicate step =
-    case step of
-        To model msgCmdList ->
-            if predicate model then
-                To model msgCmdList
-
-            else
-                Stay
-
-        Exit a ->
-            Exit a
-
-        Stay ->
-            Stay
+wrap : (to1 -> to2) -> (content1 -> content2) -> Note from to1 content1 -> Note from to2 content2
+wrap msgConstructor modelConstructor =
+    gWith msgConstructor >> edit modelConstructor
